654546b835f358f2b16ba30ea77291fe3ac494d2
just-swetha <123419834+just-swetha@users.noreply.github.com>
2026-04-25 01:57:09 -0400

    Update check-bin.js

--- Netlify/functions/check-bin.js
+++ Netlify/functions/check-bin.js
@@ -5,17 +5,19 @@ exports.handler = async (event) => {
   // 1. Get the secret key from Netlify's environment variables
   const apiKey = process.env.ROBOFLOW_API_KEY; 
   
-  // 2. Get the data sent from your HTML file
+  // 2. Parse the data from your HTML file
   const { image, model, version } = JSON.parse(event.body);
 
-  // 3. Construct the Roboflow URL (This happens on the server, hidden from the user)
+  // 3. Clean the image string (remove "data:image/jpeg;base64," if present)
+  const cleanImage = image.includes(',') ? image.split(',')[1] : image;
+
+  // 4. Construct the Roboflow URL
   const url = `https://classify.roboflow.com/${model}/${version}?api_key=${apiKey}`;
 
   try {
     const response = await fetch(url, {
       method: 'POST',
-      // Inside check-bin.js
-        body: image.includes(',') ? image.split(',')[1] : image,, // This is the base64 image from your webcam
+      body: cleanImage, // Send the cleaned string
       headers: { "Content-Type": "application/x-www-form-urlencoded" }
     });
 
@@ -23,6 +25,9 @@ exports.handler = async (event) => {
 
     return {
       statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify(data)
     };
   } catch (error) {
